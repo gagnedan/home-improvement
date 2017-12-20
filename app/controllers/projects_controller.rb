f124d3f639e4e5f7d05d6cb45b477b29a0d3d160
Daniel Gagne <gagnedan2k@gmail.com>
2017-12-20 12:29:19 -0500

    simplification du controller project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,66 +3,46 @@ class ProjectsController < ApplicationController
   before_action :authenticate_user!
   load_and_authorize_resource
 
-  # GET /projects
-  # GET /projects.json
   def index
     @projects = Project.all.accessible_by(current_ability)
   end
 
-  # GET /projects/1
-  # GET /projects/1.json
   def show
     Project.includes(:comments).find(params[:id])
   end
 
-  # GET /projects/new
   def new
     @project = Project.new
   end
 
-  # GET /projects/1/edit
   def edit
   end
 
-  # POST /projects
-  # POST /projects.json
   def create
     @project = Project.new(project_params)
     @project.user = current_user
 
-    respond_to do |format|
-      if @project.save
-        format.html { redirect_to @project, notice: 'Project was successfully created.' }
-        format.json { render :show, status: :created, location: @project }
-      else
-        format.html { render :new }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+    if @project.save
+      flash[:notice] = "Project successfully created!"
+      redirect_to @project
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /projects/1
-  # PATCH/PUT /projects/1.json
   def update
-    respond_to do |format|
-      if @project.update(project_update_params)
-        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
-        format.json { render :show, status: :ok, location: @project }
-      else
-        format.html { render :edit }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+    if @project.update(project_update_params)
+      flash[:notice] = "Project successfully updated!"
+      redirect_to @project
+    else
+      render :new
     end
   end
 
-  # DELETE /projects/1
-  # DELETE /projects/1.json
   def destroy
     @project.destroy
-    respond_to do |format|
-      format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    flash[:notice] = "Project successfully deleted!"
+    redirect_to @project
   end
 
   private
