c482c0223ec5dfe0ef4ac309578290b7b89054ff
Daniel Gagne <gagnedan2k@gmail.com>
2017-12-23 15:16:45 -0500

    Ajout de projets private et public dans les seeds

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,7 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :comments, :dependent => :destroy
 
-  validates :name, :description, :is_public, :estimated_effort, :actual_effort, :status, presence: true
+  validates :name, :description, :estimated_effort, :actual_effort, :status, presence: true
+	validates_inclusion_of :is_public, :in => [true, false]
 	validates_numericality_of :estimated_effort, :actual_effort, only_integer: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,33 @@
 
 user = User.new(
       :email => "admin@admin.com",
-      :first_name => "Daniel",
-      :last_name => "Gagné",
+      :first_name => "admin",
+      :last_name => "User",
       :password => "123456",
       :password_confirmation => "123456",
       :role => "admin" 
   )
 user.save!
+
+public_project = Project.new(
+		:name => "Project 1",
+    :description => "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.",
+    :is_public => true,
+    :estimated_effort => 8,
+    :actual_effort => 10,
+    :status => "created",
+    :user => user
+	)
+	public_project.save!
+
+private_project = Project.new(
+		:name => "Project 2",
+    :description => "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.",
+    :is_public => false,
+    :estimated_effort => 6,
+    :actual_effort => 7,
+    :status => "created",
+    :user => user
+	)
+	private_project.save!
+
