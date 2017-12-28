37be2ed68d0b9ae3478dd64b2245669a14582eca
Daniel Gagne <gagnedan2k@gmail.com>
2017-12-28 15:34:03 -0500

    mettre les champs disable que l'usager tente de modifier le nom et le statut d'un projet public dont il n'est pas owner

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,13 @@
 module ProjectsHelper
+	def is_owner?
+		if action_name == "edit" 
+			if current_user.id == @project.user.id
+				true
+			else
+				false
+			end
+		else
+			true
+		end
+	end
 end
