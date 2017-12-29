591f89e1aed3113a1251bd5df5a492fce332c8db
Daniel Gagne <gagnedan2k@gmail.com>
2017-12-28 21:37:32 -0500

    on s'assure que le estimated et actual effort sont entre 1 et 10

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,5 +5,6 @@ class Project < ApplicationRecord
   
   validates :name, :description, :estimated_effort, :actual_effort, :status, presence: true
 	validates_inclusion_of :is_public, :in => [true, false]
-	validates_numericality_of :estimated_effort, :actual_effort, only_integer: true
+	validates_inclusion_of :estimated_effort, :actual_effort, :in => 1..10
+	validates_numericality_of :estimated_effort, :actual_effort, only_integer: true, :in => 1..10
 end
