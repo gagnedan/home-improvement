08501d8f476522f5d99d7fe019b57379d97edf7a
Daniel Gagne <gagnedan2k@gmail.com>
2017-12-29 00:16:41 -0500

    move project to partials

--- app/assets/javascripts/channels/project.js
+++ app/assets/javascripts/channels/project.js
@@ -1,20 +1,22 @@
+// self invoke function
+
 (function() {
-    // Subscribe to the class name of the channel
-    App.projects = App.cable.subscriptions.create('ProjectsChannel', {
-        /**
-         * Whenever this channel pushes content, it is received here
-         */
-        received: function(project) {
-            
-            var $project = $('[data-project-id="' + project.id + '"]');
-            if ($project.length > 0) {
-                // Existing Task
-                switch (project.response) {
-                    case 'deleted':
-                        $project.remove();
-                        break;
-                }
-            } 
+  // Subscribe
+  App.projects = App.cable.subscriptions.create('ProjectsChannel', {
+    
+    // channel receiver
+    received: function(project) {
+
+    var $project = $('[data-project-id="' + project.id + '"]');
+      if ($project.length > 0) {
+      
+        switch (project.response) {
+
+          case 'deleted':
+              $project.remove();
+              break;
         }
-    });
+      } 
+    }
+  });
 }).call(this);
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,18 +1,18 @@
 class Project < ApplicationRecord
+  
   enum status: [:created, :started, :stopped, :completed]
   belongs_to :user
   has_many :comments, :dependent => :destroy
 
+  validates :name, :description, :estimated_effort, :actual_effort, :status, presence: true
+  validates_inclusion_of :is_public, :in => [true, false]
+  validates_inclusion_of :estimated_effort, :actual_effort, :in => 1..10
+  validates_numericality_of :estimated_effort, :actual_effort, only_integer: true, :in => 1..10
+
   after_destroy :broadcast_delete
 
-def broadcast_delete
+  def broadcast_delete
     ActionCable.server.broadcast 'projects', response: 'deleted', id: id
   end
-
-
-  validates :name, :description, :estimated_effort, :actual_effort, :status, presence: true
-	validates_inclusion_of :is_public, :in => [true, false]
-	validates_inclusion_of :estimated_effort, :actual_effort, :in => 1..10
-	validates_numericality_of :estimated_effort, :actual_effort, only_integer: true, :in => 1..10
-
+  
 end
